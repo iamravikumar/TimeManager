9b96261e3b16e2280b841e1afb5056c69511da1f
Murat Demirci <mdemirci@outlook.com>
2018-01-16 12:20:39 +0300

    security

--- TimeManager/Controllers/HomeController.cs
+++ TimeManager/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TimeManager.Data;
@@ -10,6 +11,7 @@ using TimeManager.Models;
 
 namespace TimeManager.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext context;
